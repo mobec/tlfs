b7a61c40ca4079093e2d652e6fa321bc19712762
Moritz Becher <git@mo-becher.de>
2018-12-15 14:43:29 +0100

    orthogonality regularizer

---
+++ extensions/regularizers.py
@@ -0,0 +1,42 @@
+#
+#   Copyright 2018 Moritz Becher
+#
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+import keras
+import keras.regularizers
+import keras.backend as K
+
+class ConvolutionOrthogonality(keras.regularizers.Regularizer):
+    def __init__(self, factor: float = 1.0):
+        self.factor =  K.cast_to_floatx(factor)
+
+    def __call__(self, kernel):
+        # 1.) Flatten the weight matrix
+        w = K.reshape(kernel, shape=(-1,))
+        # 2.) construct a square matrix consisting of w repeated in the rows and clear the diagonal
+        o = K.squeeze(K.repeat(K.expand_dims(w, -1) ,w.shape[0]), -1) - K.eye(w.shape[0].value) * w
+        # 3.) the regularizer is the L1 norm of the product divided by two, to account for double entries from the
+        # symmetric matrix o
+        w = K.expand_dims(w, -1)
+        result = self.factor / 2.0 * K.sum(K.abs(K.dot(o, w)))
+        return result
+
+    def get_config(self):
+        return {'factor': float(self.factor)}
+
+
+def ortho(factor: float = 1.0):
+    return ConvolutionOrthogonality(factor)
--- models/vgg.py
+++ models/vgg.py
@@ -59,25 +59,25 @@ class VGG(Network):
         # ----------------------------------------------------------------------------------
         x = k.layers.Input(shape=self.input_shape)
         # ----------------------------------------------------------------------------------
-        h = k.layers.Conv2D(64,  (3, 3), padding='same', trainable=True)(x)
+        h = k.layers.Conv2D(64,  (3, 3), padding='same', trainable=True, kernel_regularizer=e.regularizers.ortho())(x)
         h = k.layers.Activation('relu')(h)
         # ----------------------------------------------------------------------------------
-        h = k.layers.Conv2D(64, (3, 3), padding='same', trainable=True)(h)
+        h = k.layers.Conv2D(64, (3, 3), padding='same', trainable=True, kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
         # ----------------------------------------------------------------------------------
         h = k.layers.MaxPool2D((2,2))(h)
 
         #----------------------------------------------------------------------------------
-        h = k.layers.Conv2D(128, (3, 3), padding='same', trainable=True)(h)
+        h = k.layers.Conv2D(128, (3, 3), padding='same', trainable=True, kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
         # ----------------------------------------------------------------------------------
-        h = k.layers.Conv2D(128, (3, 3), padding='same', trainable=True)(h)
+        h = k.layers.Conv2D(128, (3, 3), padding='same', trainable=True, kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
         # ----------------------------------------------------------------------------------
         h = k.layers.MaxPool2D((2, 2))(h)
 
         #----------------------------------------------------------------------------------
-        h = k.layers.Conv2D(256, (3, 3), padding='same', trainable=True)(h)
+        h = k.layers.Conv2D(256, (3, 3), padding='same', trainable=True, kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
         # # ----------------------------------------------------------------------------------
         # h = k.layers.Conv2D(256, (3, 3), padding='same', trainable=True)(h)
@@ -100,27 +100,27 @@ class VGG(Network):
         # h = k.layers.Conv2DTranspose(256, (3, 3), padding='same')(h)
         # h = k.layers.Activation('relu')(h)
         # ----------------------------------------------------------------------------------
-        h = k.layers.Conv2DTranspose(128, (3, 3), padding='same')(h)
+        h = k.layers.Conv2DTranspose(128, (3, 3), padding='same', kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
 
         #----------------------------------------------------------------------------------
         #h = e.layers.InvMaxPool2D((2, 2))(h)
         h = k.layers.UpSampling2D((2, 2))(h)
         # ----------------------------------------------------------------------------------
-        h = k.layers.Conv2DTranspose(128, (3, 3), padding='same')(h)
+        h = k.layers.Conv2DTranspose(128, (3, 3), padding='same', kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
         # ----------------------------------------------------------------------------------
-        h = k.layers.Conv2DTranspose(64, (3, 3), padding='same')(h)
+        h = k.layers.Conv2DTranspose(64, (3, 3), padding='same', kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
 
         #----------------------------------------------------------------------------------
         #h = e.layers.InvMaxPool2D((2, 2))(h)
         h = k.layers.UpSampling2D((2, 2))(h)
         # ----------------------------------------------------------------------------------
-        h = k.layers.Conv2DTranspose(64, (3, 3), padding='same')(h)
+        h = k.layers.Conv2DTranspose(64, (3, 3), padding='same', kernel_regularizer=e.regularizers.ortho())(h)
         h = k.layers.Activation('relu')(h)
         # ----------------------------------------------------------------------------------
-        y = k.layers.Conv2DTranspose(3, (3, 3), padding='same')(h)
+        y = k.layers.Conv2DTranspose(3, (3, 3), padding='same', kernel_regularizer=e.regularizers.ortho())(h)
 
         self.model = k.models.Model(inputs=x, outputs=y)
 
@@ -202,6 +202,7 @@ class VGG(Network):
             train_steps_per_epoch = dataset.train.steps_per_epoch(batch_size=batch_size, augment=augment)
             val_generator = dataset.val.generator(batch_size=batch_size, augment=augment, noise=False)
             val_steps = dataset.val.steps_per_epoch(batch_size=batch_size, augment=augment)
+            print(val_steps)
             hist = self.model.fit_generator(
                 generator=train_generator,
                 steps_per_epoch=train_steps_per_epoch,
