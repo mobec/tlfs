1e487055618c79ef6ac47c1dd0b1facc51138b57
Moritz Becher <git@mo-becher.de>
2018-12-15 15:07:44 +0100

    reduced memory requirements

--- extensions/regularizers.py
+++ extensions/regularizers.py
@@ -18,20 +18,25 @@
 import keras
 import keras.regularizers
 import keras.backend as K
+import tensorflow as tf
 
 class ConvolutionOrthogonality(keras.regularizers.Regularizer):
     def __init__(self, factor: float = 1.0):
         self.factor =  K.cast_to_floatx(factor)
 
     def __call__(self, kernel):
-        # 1.) Flatten the weight matrix
-        w = K.reshape(kernel, shape=(-1,))
-        # 2.) construct a square matrix consisting of w repeated in the rows and clear the diagonal
-        o = K.squeeze(K.repeat(K.expand_dims(w, -1) ,w.shape[0]), -1) - K.eye(w.shape[0].value) * w
-        # 3.) the regularizer is the L1 norm of the product divided by two, to account for double entries from the
-        # symmetric matrix o
-        w = K.expand_dims(w, -1)
-        result = self.factor / 2.0 * K.sum(K.abs(K.dot(o, w)))
+        ks = tf.split(kernel, kernel.shape[-1], axis=-1)
+        result = 0.0
+        for k in ks:
+            k = K.squeeze(k, -1)
+            # 1.) Flatten the weight matrix
+            w = K.reshape(k, shape=(-1,))
+            # 2.) construct a square matrix consisting of w repeated in the rows and clear the diagonal
+            o = K.squeeze(K.repeat(K.expand_dims(w, -1) ,w.shape[0]), -1) - K.eye(w.shape[0].value) * w
+            # 3.) the regularizer is the L1 norm of the product divided by two, to account for double entries from the
+            # symmetric matrix o
+            w = K.expand_dims(w, -1)
+            result += self.factor / 2.0 * K.sum(K.abs(K.dot(o, w)))
         return result
 
     def get_config(self):
