0de33fafc16926a75e8aa7796c95ae8dc6ecdcf5
Moritz Becher <git@mo-becher.de>
2018-12-27 12:23:44 +0100

    further decreased learning rate

--- models/vgg.py
+++ models/vgg.py
@@ -39,7 +39,7 @@ class VGG(Network):
     def _init_vars(self, **kwargs):
         self.init_func = "glorot_normal"
         self.adam_epsilon = None #1e-8 # 1e-3
-        self.adam_learning_rate = 0.0005 # higher values tend to overshoot in the beginning
+        self.adam_learning_rate = 0.0001 # higher values tend to overshoot in the beginning
         self.adam_weight_decay = 0.000#1e-5
         self.input_shape = kwargs.get("input_shape", (64, 64, 64, 1))
         self.loss = "mse"
