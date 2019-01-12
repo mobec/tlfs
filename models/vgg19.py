57ed7fbd2c7c6f9fbddab7c9b08bc084ea70f1d6
Moritz Becher <git@mo-becher.de>
2019-01-12 14:25:33 +0100

    image net vgg19

---
+++ models/vgg19.py
@@ -0,0 +1,193 @@
+###############################################################################
+#
+#   Copyright 2018 Moritz Becher
+#
+#   abstract network layout class
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+###############################################################################
+
+
+import tensorflow as tf
+import keras as k
+import extensions as e
+import numpy as np
+
+from models.architecture import Network
+
+def cast_shape_to(shape, type):
+    out_shape= []
+    for i in shape:
+        try:
+            out_shape.append(type(i))
+        except:
+            out_shape.append(None)
+    return out_shape
+
+class VGG19(Network):
+    def _init_vars(self, **kwargs):
+        self.init_func = "glorot_normal"
+        self.adam_epsilon = None #1e-8 # 1e-3
+        self.adam_learning_rate = 0.00001 # higher values tend to overshoot in the beginning
+        self.adam_weight_decay = 0.005#1e-5
+        self.input_shape = kwargs.get("input_shape", (64, 64, 64, 1))
+        self.classes = kwargs.get("classes", 1000)
+        self.loss = k.losses.categorical_crossentropy
+        self.metrics = [k.metrics.categorical_accuracy]
+        self.variational_ae = False
+        self.kl_beta = 1e-5
+        self.tensorflow_seed = kwargs.get("tensorflow_seed", 1337)
+        self.model = None
+        self.ortho_regularizer_strength = kwargs.get("ortho_strength", 0.01)
+        self.kernel_regularizer = None
+        if kwargs.get("ortho_regularizer", False):
+            self.kernel_regularizer = e.regularizers.ortho(self.ortho_regularizer_strength)
+        tf.set_random_seed(self.tensorflow_seed)
+        np.random.seed(1337)
+
+    def _init_optimizer(self, epochs=1):
+        self.optimizer = k.optimizers.Adam(lr=self.adam_learning_rate, epsilon=self.adam_epsilon, decay=self.adam_weight_decay)
+        return self.optimizer
+
+    def _build_model(self):
+        self.add_custom_object(e.regularizers.ConvolutionOrthogonality)
+
+        x = k.layers.Input(shape=self.input_shape)
+
+        # Block 1
+        h = k.layers.Conv2D(64, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block1_conv1')(x)
+        h = k.layers.Conv2D(64, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block1_conv2')(h)
+        h = k.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(h)
+
+        # Block 2
+        h = k.layers.Conv2D(128, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block2_conv1')(h)
+        h = k.layers.Conv2D(128, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block2_conv2')(h)
+        h = k.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(h)
+
+        # Block 3
+        h = k.layers.Conv2D(256, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block3_conv1')(h)
+        h = k.layers.Conv2D(256, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block3_conv2')(h)
+        h = k.layers.Conv2D(256, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block3_conv3')(h)
+        h = k.layers.Conv2D(256, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block3_conv4')(h)
+        h = k.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(h)
+
+        # Block 4
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block4_conv1')(h)
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block4_conv2')(h)
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block4_conv3')(h)
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block4_conv4')(h)
+        h = k.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(h)
+
+        # Block 5
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block5_conv1')(h)
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block5_conv2')(h)
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block5_conv3')(h)
+        h = k.layers.Conv2D(512, (3, 3),
+                          activation='relu',
+                          padding='same',
+                          name='block5_conv4')(h)
+        h = k.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(h)
+
+        # Classification block
+        h = k.layers.Flatten(name='flatten')(h)
+        h = k.layers.Dense(4096, activation='relu', name='fc1')(h)
+        h = k.layers.Dense(4096, activation='relu', name='fc2')(h)
+        y = k.layers.Dense(self.classes, activation='softmax', name='predictions')(h)
+
+        self.model = k.models.Model(inputs=x, outputs=y)
+
+    def _compile_model(self):
+        self.model.compile(loss=self.loss, optimizer=self.optimizer, metrics=self.metrics)
+
+    def save_model(self, path):
+        # search for layers with custom regularizer and remove it from them
+        layers_with_ortho = []
+        for layer in self.model.layers:
+            if hasattr(layer, "kernel_regularizer") and isinstance(layer.kernel_regularizer, e.regularizers.ConvolutionOrthogonality):
+                factor = layer.kernel_regularizer.factor
+                layers_with_ortho.append((layer, factor))
+                layer.kernel_regularizer = None
+        # save the model without the custom regularizer
+        super().save_model(path)
+        # restore the original regularizer
+        for layer, factor in layers_with_ortho:
+            layer.kernel_regularizer = e.regularizers.ConvolutionOrthogonality(factor)
+
+    def _train(self, epochs, **kwargs):
+        if epochs == 0:
+            return None
+
+        images, labels = kwargs["data"]
+        batch_size = kwargs.get("batch_size", 32)
+        augment = kwargs.get("augment", False)
+        hist = None
+        try:
+            hist = self.model.fit(
+                x=images,
+                y=labels,
+                validation_split=0.1,
+                epochs=epochs,
+                max_queue_size=100
+            )
+        except KeyboardInterrupt:
+            print("Interrupted by user")
+        return hist
+
+    def predict(self, x, batch_size):
+        return self.model.predict(x, batch_size=batch_size)
---
+++ vgg19.py
@@ -0,0 +1,131 @@
+###############################################################################
+#
+#   Copyright 2018 Moritz Becher
+#
+#   abstract network layout class
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+###############################################################################
+
+import os
+import math
+import pickle
+
+from models.vgg19 import VGG19
+from util.plot import Plotter
+import numpy as np
+np.random.seed(4)
+
+def train_vgg19(dataset_path, model_path, epochs, ortho=False, ortho_factor=0.1):
+    images = []
+    labels = []
+    for dataset_file in os.listdir(dataset_path + "/train"):
+        with open(dataset_path + "/train/" + dataset_file, "rb") as f:
+            d = pickle.load(f)
+
+        images.append(d['data'])
+        labels.append(d['labels'])
+
+    images = np.concatenate(images).astype(np.float32)
+    labels = np.concatenate(labels)
+
+    #print(images.shape)
+    img_dim = int(math.sqrt(images.shape[-1] / 3))
+    images.resize((images.shape[0], img_dim, img_dim, 3))
+
+    normalization_shift = np.mean(images, axis=(0, 1, 2))
+    normalization_factor = np.std(images, axis=(0, 1, 2))
+    normalization_factor[-1] = 1.0
+    normalization_factor *= 1.0 / 255.0
+
+    images += normalization_shift
+    images /= normalization_factor
+
+    hist = {}
+
+    ae = VGG19(input_shape=(img_dim, img_dim, 3), ortho_regularizer=ortho, ortho_strength=ortho_factor)
+    # if os.path.isfile(model_path):
+    #     ae.load_model(path=model_path)
+
+    hist = ae.train(epochs, data=(images, labels), batch_size=32)
+    ae.save_model(path=model_path)
+
+    return hist
+
+
+# def predict_test_data(dataset_path, model_path):
+#     dataset = DataSet()
+#     dataset.load(path=dataset_path, blocks=["velocity"], shuffle=False)
+#     normalization_shift = np.mean(dataset.train.velocity.data, axis=(0, 1, 2))
+#     normalization_factor = np.std(dataset.train.velocity.data, axis=(0, 1, 2))
+#     normalization_factor[-1] = 1.0
+#     normalization_factor *= 1.0 / 255.0
+#     dataset.test.velocity.normalize(shift=normalization_shift, factor=normalization_factor)
+#
+#     ae = VGG(input_shape=(None, None, 3))
+#
+#     ae.load_model(path=model_path)
+#
+#     orig = dataset.test.velocity
+#     pred = ae.predict(orig.data, batch_size=8)
+#     orig.denormalize()
+#     pred *= normalization_factor
+#     pred -= normalization_factor
+#
+#     return orig.data, pred
+
+
+if __name__ == '__main__':
+    try:
+        import argparse
+        import os
+        import json
+
+        parser = argparse.ArgumentParser(description="Train the tlfs model")
+        parser.add_argument("-o", "--output", type=str, required=True, help="The output path")
+        parser.add_argument("-d", "--dataset", type=str, required=True, help="The dataset path")
+        parser.add_argument("--train", action="store_true", help="Train the model")
+        parser.add_argument("--test", action="store_true", help="Test the model")
+        parser.add_argument("--gui", action="store_true", help="Test the model")
+        parser.add_argument("--epochs", type=int, default=50, help="The number of training epochs")
+        parser.add_argument("--ortho_regularization", action="store_true", help="Orthogonality regularization")
+        parser.add_argument("--ortho_factor", type=float, default=0.1, help="Strength of the orthogonality regularization")
+        parser.add_argument("model", type=str, help="The path to the model file (.h5)")
+        args = parser.parse_args()
+
+        os.makedirs(args.output, exist_ok=True)
+
+        plot = Plotter()
+
+        if args.train:
+            hist = train_vgg19(args.dataset, args.model, args.epochs, args.ortho_regularization, args.ortho_factor)
+            if hist:
+                with open(args.output + "/hist.json", 'w') as f:
+                    json.dump(hist.history, f)
+                plot.plot_history(hist.history, log_y=True)
+
+        # if args.test:
+        #     originals, predictions = predict_test_data(args.dataset, args.model)
+        #     for o, p in zip(originals, predictions):
+        #         plot.plot_vector_field(o, p, title="Test Prediction", scale=300.0)
+
+        # if args.gui:
+        #     plot.show(True)
+        #
+        # figures_path = args.output + "/figures"
+        # os.makedirs(figures_path, exist_ok=True)
+        # plot.save_figures(figures_path)
+    finally:
+        import util.pushover
+        util.pushover.notify("Training complete")
